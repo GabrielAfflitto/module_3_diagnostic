f3a3aecb352ab32a9ae05701decc2cd6296a0fd7
GabrielAfflitto <gafflitto92@gmail.com>
2018-02-07 10:16:28 -0700

    still working on test, might have fixed issue with endpoint in test

--- spec/features/user_can_search_by_zipcode_spec.rb
+++ spec/features/user_can_search_by_zipcode_spec.rb
@@ -4,19 +4,19 @@ describe "When a user visits the root page" do
   it "should allow the user to search by a zipcode" do
     visit root_path
 
-    # save_and_open_page
     fill_in "q", with: "80203"
     click_on "Locate"
 
-    conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?") do |faraday|
+    expect(current_path).to eq(search_path)
+
+    conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1") do |faraday|
       faraday.headers['api_key'] = ENV["API_KEY"]
-      faraday.adapter Faraday.default_adapter
+      faraday.headers['Accept'] = 'application/json'
+      faraday.adapter    Faraday.default_adapter
     end
 
-    response = conn.get("/location=80203&fuel_type=ELEC&LPG")
+    response = conn.get("nearest?&location=80219&fuel_type=ELEC&LPG")
     stations = JSON.parse(response.body)
-
-    expect(current_path).to eq(search_path)
     # expect(page).to have_content
   end
 end
